6cfa9833633c0bbbcdc0a5c65da3eaf7e8b4d88a
Liam Ilan <snowboardsheep@gmail.com>
2021-09-26 14:46:15 -0700

    png to jpg.

--- scripts/data.js
+++ scripts/data.js
@@ -2,119 +2,119 @@
 
 const data = [
   {
-    image: '../img/oddify.png',
+    image: '../img/oddify.jpg',
     title: 'Oddify',
     time: '[2021]',
     description: "A discord bot built to paste Oddish's face onto random Pokemon, flags and pfps.",
     link: 'https://github.com/liam-ilan/oddify'
   },
   {
-    image: '../img/clyde.png',
+    image: '../img/clyde.jpg',
     title: 'Clyde',
     time: '[2020]',
     description: 'A language developed with Pranav Karthik that helps you to build a discord bot.',
     link: 'https://repl.it/talk/challenge/Clyde-The-Discord-Bot-Programming-Language/51541'
   },
   {
-    image: '../img/fmdb.png',
+    image: '../img/fmdb.jpg',
     title: 'FMDB',
     time: '[2020]',
     description: 'An app that helps a family keep a record of movies watched, created with Java.',
     link: 'https://github.com/liam-ilan/fmdb'
   },
   {
-    image: '../img/hauk.png',
+    image: '../img/hauk.jpg',
     title: 'The HAUK Project',
     time: '[2020]',
     description: 'A site that generates stories using Markov Chains.',
     link: 'https://github.com/liam-ilan/sad-story-generator'
   },
   {
-    image: '../img/virus-simulator.png',
+    image: '../img/virus-simulator.jpg',
     title: 'Virus Simulator',
     time: '[2020]',
     description: 'A playground to simulate the spread of a virus.',
     link: 'https://github.com/liam-ilan/virus-simulator'
   },
   {
-    image: '../img/ccc-challenge.png',
+    image: '../img/ccc-challenge.jpg',
     title: 'CCC Challenge',
     time: '[2020]',
     description: 'A collection of completed CCC problems.',
     link: 'https://github.com/liam-ilan/ccc-challenge'
   },
   {
-    image: '../img/skin-cancer.png',
+    image: '../img/skin-cancer.jpg',
     title: 'Skin Cancer Classifier',
     time: '[2020]',
     description: 'A tool for training Teachable Machine models to classify skin cancer.',
     link: 'https://github.com/liam-ilan/science-fair'
   },
   {
-    image: '../img/repl-hero.png',
+    image: '../img/repl-hero.jpg',
     title: 'Repl Hero',
     time: '[2019]',
     description: 'Guitar Hero... FOR HACKERS.',
     link: 'https://repl.it/talk/challenge/Repl-Hero-a-fun-music-arcade-game/16400'
   },
   {
-    image: '../img/sendverse.png',
+    image: '../img/sendverse.jpg',
     title: 'Sendverse',
     time: '[2019]',
     description: 'An anonymous chatting server you can deploy for your friends.',
     link: 'https://github.com/liam-ilan/sendverse'
   },
   {
-    image: '../img/monty-hall.png',
+    image: '../img/monty-hall.jpg',
     title: 'The Monty Hall Problem',
     time: '[2018 - 2019]',
     description: 'An interactive simulation of the Monty Hall Problem.',
     link: 'https://github.com/liam-ilan/monty-hall-problem'
   },
   {
-    image: '../img/forest.png',
+    image: '../img/forest.jpg',
     title: 'Forest Generation',
     time: '[2018]',
     description: 'A cellular automata for generating trees in your command line.',
     link: 'https://github.com/liam-ilan/forest-generation-cellular-automata'
   },
   {
-    image: '../img/sticky-quest.png',
+    image: '../img/sticky-quest.jpg',
     title: 'Sticky Quest',
     time: '[2018]',
     description: 'A simple block in a quest for love.',
     link: 'https://itunes.apple.com/ca/app/sticky-quest/id1425679649'
   },
   {
-    image: '../img/wolfram.png',
+    image: '../img/wolfram.jpg',
     title: "Wolfram's Cellular Automata",
     time: '[2018]',
     description: "An implementation of Wolfram's cellular automata in Node.js",
     link: 'https://github.com/liam-ilan/wolfram-cellular-automata'
   },
   {
-    image: '../img/game-of-life.png',
+    image: '../img/game-of-life.jpg',
     title: 'Game of Life',
     time: '[2018]',
     description: 'An implementation of the Game of Life in Node.js.',
     link: 'https://github.com/liam-ilan/game-of-life'
   },
   {
-    image: '../img/do-not-shear-the-sheep.png',
+    image: '../img/do-not-shear-the-sheep.jpg',
     title: 'Do Not Shear The Sheep',
     time: '[2018]',
     description: 'Try to dodge the shears. A simple arcade game.',
     link: 'http://do-not-shear-the-sheep.surge.sh/'
   },
   {
-    image: '../img/asteroid-dodge.png',
+    image: '../img/asteroid-dodge.jpg',
     title: 'Asteroid Dodge',
     time: '[2017]',
     description: 'Just dodge the asteroids.',
     link: 'https://asteroid-dodge.surge.sh'
   },
   {
-    image: '../img/the-train-game.png',
+    image: '../img/the-train-game.jpg',
     title: 'The Train Game',
     time: '[2017]',
     description: 'A game about stopping trains.',
